562effe7f3ff7c5299900215432c4c194a302cda
Catherine Yount <catherine.yount@g,austincc.edu>
2023-12-31 11:56:10 -0600

    Fixed exp inital class & funcs created

--- finance_tools/fixed_expenses.py
+++ finance_tools/fixed_expenses.py
@@ -1,11 +1,31 @@
-class FixedExpenses:
-    def __init__(self, expenses=None):
-        self.expenses = expenses if expenses is not None else {}
+class Fixed_Expenses:
+    def __init__(self):
+        self.fixed_expenses = []
 
-    def add_expense(self, name, amount):
-        self.expenses[name] = amount
-
-    def total_expenses(self):
-        return sum(self.expenses.values())
+    def get_fixed_expenses(self):
+        return sum(Fixed_Expenses['Amount'])
     
-        ## TODO ## finish crud, ref investments.py
+    def add_fixed_expenses(self, expense, amount, note):
+        self.fixed_expenses.append({
+            'Expense': expense,
+            'Amount': amount,
+            'Note': note
+        })
+        
+    def update_fixed_expenses(self, expense , amount, note):
+        for expense in self.fixed_expenses:
+               if expense['Expense'].lower() == expense.lower():
+                    expense['Amount'] = amount
+                    expense['Note'] = note
+                    return f"Fixed Expenses updated for Expense: {expense}"
+        return f"Fixed Expenses could not update for Expense: {expense}"
+    
+    def delete_fixed_expenses(self, expense):
+            self.fixed_expenses = [i for i in self.fixed_expenses if i['Expense'].lower() != expense.lower()]
+            return f"Fixed Expenses deleted for expense: {expense}"
+
+my_fixed_expenses = Fixed_Expenses()
+
+my_fixed_expenses.add_fixed_expenses('Netflix', 12.50, 'subscription')
+my_fixed_expenses.delete_fixed_expenses('Emergency Fund')
+
